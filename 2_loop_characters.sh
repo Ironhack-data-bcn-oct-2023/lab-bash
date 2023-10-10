0584231cf8fe208a8811917ac5314189dca712bd
Amir Golafshan <amirg33@outlook.com>
2023-10-10 09:41:34 +0200

    folder name changed

--- 2_loop_characters.sh
+++
@@ -1,6 +0,0 @@
-find lorem/ -name "*.csv" |\
-while read path; do
-    $file=$(basename "$path")
-    $len=$(echo -n "$file" | wc -c) 
-    echo $len "$path"
-done
--- loop_characters.sh
+++
@@ -1,6 +0,0 @@
-for i in 'ls -l'
-do
-	val=`echo $i | cut -d '.' -f1 | wc | tr '\t' ' ' | tr -s ' ' | sed 's/^ //' | cut -d' ' -f 3`
-	val=`/usr/bin/expr $val - 1`
-	echo $i $val
-done
